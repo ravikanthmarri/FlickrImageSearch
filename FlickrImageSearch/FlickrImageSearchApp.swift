01a557c3b7db4ea703bd28e0af2da5861e947f39
Ravikanth Marri <ravikanth.marri@gmail.com>
2024-02-22 19:53:02 +0000

    Initial Commit

---
+++ FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchApp.swift
+//  FlickrImageSearch
+//
+//  Created by Ravikanth on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
